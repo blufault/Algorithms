f0ca4e75572ac8a438f0d52265e614ce8058d4b5
Jeffrey Kim <heffy.etc@gmail.com>
2019-12-12 14:07:03 -0800

    added gitmodules and removed my Iterator header

--- Algorithms/Iterator/Iterator.h
+++
@@ -1,8 +0,0 @@
-
-// Iterator
-// An iterator range: denoted as pair of iterators each of which refers to an element
-// These iterators: begin and end
-
-template <class Item> class Iterator {
-  private:
-};
