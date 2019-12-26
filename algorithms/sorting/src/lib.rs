19b2885f3969eccc57e6b972fc2f8fa5d102da6c
Jeffrey Kim <heffy.etc@gmail.com>
2019-12-25 23:13:45 -0800

    binary search returns a Result

--- algorithms/sorting/src/lib.rs
+++ algorithms/sorting/src/lib.rs
@@ -4,11 +4,11 @@ pub use crate::sorting::binary_search;
 #[test]
 fn should_be_correct() {
     let v: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13];
-    assert_eq!(3, binary_search::binary_search(&v, 4));
+    assert_eq!(Ok(3), binary_search::binary_search(&v, 4));
 }
 
 #[test]
 fn should_be_incorrect() {
     let v: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13];
-    assert_eq!(-1, binary_search::binary_search(&v, 20));
+    assert_eq!(Err(-1), binary_search::binary_search(&v, 20));
 }
--- algorithms/sorting/src/sorting.rs
+++ algorithms/sorting/src/sorting.rs
@@ -1,17 +1,21 @@
 
 // TODO:
+// [x] - Implement Result<Ok, Err>
 // [] - Implement Generics for binary_search
 // [] - Since Rust does not support overloading functions, try enums with tuples, traits and impl
 
 pub mod binary_search {
-    pub fn binary_search(v: &Vec<i32>, target: i32) -> i32 {
+    pub fn binary_search(v: &Vec<i32>, target: i32) -> Result<i32, i32> {
         let len_sub_one = (v.len() - 1) as usize;
-        return binary_search_impl(&v, target, 0, len_sub_one);
+        return Ok(binary_search_impl(&v, target, 0, len_sub_one)).unwrap_or_else(|err| {
+            println!("Reached inside unwrap or else...\nError is {}", err);
+            return Err(err);
+        });
     }
 
-    fn binary_search_impl(v: &Vec<i32>, target: i32, low: usize, high: usize) -> i32 {
+    fn binary_search_impl(v: &Vec<i32>, target: i32, low: usize, high: usize) -> Result<i32, i32>{
         if low > high {
-            return -1;
+            return Err(-1);
         }
 
         let mid: usize = low + (high - low) / 2;
@@ -21,7 +25,7 @@ pub mod binary_search {
         } else if target > v[mid] {
             return binary_search_impl(v, target, mid + 1, high);
         } else {
-            return mid as i32;
+            return Ok(mid as i32);
         }
     }
 }
