2c1055ca6594c2214654e8fc98b25558514bf50c
Jeffrey Kim <heffy.etc@gmail.com>
2019-12-27 17:48:04 -0800

    writing test for stack of strings

--- algorithms/data_structure/src/fixed_capacity/mod.rs
+++ algorithms/data_structure/src/fixed_capacity/mod.rs
@@ -1,16 +1,41 @@
 
-pub mod fixed_capacity {
-    pub mod Point {
-        pub struct Point {
-            pub x: i32,
-            pub y: i32
+use std::io::Error;
+
+pub mod stack_of_strings {
+    pub struct Stack {
+        storage: [i32; 5],
+        len: usize
+    }
+
+    impl Stack {
+        pub fn new() -> Stack {
+            Stack { len: 0, storage: [0, 0, 0, 0, 0] }
         }
 
-        impl Point {
-            pub fn new() -> Point {
-                Point { x: 0, y: 0 }
-            }
+        pub fn getLen(&self) -> usize {
+            self.len
         }
+
+        pub fn push(&mut self, value: i32) {
+            self.storage[self.len] = value;
+            // Add a trait to allow Binary Operation
+            self.len = self.len + 1;
+        }
+
+        pub fn pop(&mut self) -> i32 {
+            // Add a trait to allow Binary Operation
+            self.len = self.len - 1;
+            let val: i32 = self.storage[self.len];
+            val
+        }
+
+        pub fn at(&self, ind: usize) -> i32 {
+            self.storage[ind]
+        }
+
+        // pub fn peek(&self) -> i32 {}
+
+        // fn range_check() -> Result<i32, io::Error> {}
     }
 }
 
--- algorithms/data_structure/src/lib.rs
+++ algorithms/data_structure/src/lib.rs
@@ -1,11 +1,59 @@
 
 mod fixed_capacity;
-pub use crate::fixed_capacity::fixed_capacity::Point::Point;
+pub use crate::fixed_capacity::stack_of_strings::Stack;
 
 #[test]
-fn test() {
-    let p = Point::new();
-
-    assert_eq!(p.x, 0);
+fn init_fixed_capacity_stack_of_strings() {
+    let s: Stack = Stack::new();
+    assert_eq!(s.getLen(), 0);
 }
 
+#[test]
+fn stack_of_strings_push() {
+    let mut s: Stack = Stack::new();
+    s.push(1);
+
+    assert_eq!(s.getLen(), 1);
+    assert_eq!(s.at(0), 1);
+}
+
+#[test]
+fn stack_of_strings_push_p_2() {
+    let mut s: Stack = Stack::new();
+    s.push(1);
+    s.push(2);
+
+    assert_eq!(s.getLen(), 2);
+    assert_eq!(s.at(1), 2);
+}
+
+#[test]
+fn stack_of_string_pop() {
+    let mut s: Stack = Stack::new();
+    s.push(1);
+    s.push(2);
+    let ret = s.pop();
+
+    assert_eq!(s.getLen(), 1);
+    assert_eq!(s.at(0), 1);
+    assert_eq!(ret, 2);
+}
+
+#[test]
+fn stack_of_strings_pop_p_2() {
+    let mut s: Stack = Stack::new();
+    s.push(1);
+    s.push(2);
+    let ret = s.pop();
+    let ret2 = s.pop();
+
+    assert_eq!(s.getLen(), 0);
+    assert_eq!(ret, 2);
+    assert_eq!(ret2, 1);
+}
+
+#[test]
+fn stack_of_string_invalid_pop() {
+    let mut s: Stack = Stack::new();
+    s.pop();
+}
